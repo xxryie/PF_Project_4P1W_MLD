55ebea8cd5ecc0f7d0f824558909294e215b757d
Xyrylle Matiga <c2097-23@itmlyceumalabang.onmicrosoft.com>
2026-03-29 11:20:22 +0800

    remove

--- auth-api/DTOs/AuthDtos.cs
+++
@@ -1,5 +0,0 @@
-namespace auth_api.DTOs;
-
-public record RegisterRequest(string Email, string Password);
-public record LoginRequest(string Email, string Password);
-public record AuthResponse(string Token, string Email, string Role);
